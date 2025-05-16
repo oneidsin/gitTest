c51860bd2ca1085bf8b082a55cdb4905778e1a04
oneidsin <oneidsin@naver.com>
2025-05-16 15:41:10 +0900

    브랜치 초기화 설명

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -14,6 +14,7 @@ public class MainController {
 		
 		logger.info("hello 신혁재");
 		logger.info("아무거나 추가");
+		logger.info("브랜치 초기화 : 특정한 커밋의 상태로 완전히 되돌려 버리는 것.");
 		return "{\"msg\":\"OK\"}";
 	}
 	
