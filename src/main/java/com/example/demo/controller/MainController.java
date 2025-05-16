60b0866c2947fcf69593d98555538d07bd4f4297
oneidsin <oneidsin@naver.com>
2025-05-16 15:10:09 +0900

    로거 추가

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@GetMapping(value = "/")
 	public String main() {
+		
+		logger.info("hello");
 		return "{\"msg\":\"OK\"}";
 	}
 }
