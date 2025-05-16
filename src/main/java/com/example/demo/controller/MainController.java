b46066aa448021907a5b2b398ac37e0bbd0c840a
oneidsin <oneidsin@naver.com>
2025-05-16 15:47:51 +0900

    리버스 커밋에서 충돌난 부분 해결 후 적용

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -13,9 +13,9 @@ public class MainController {
 	public String main() {
 		
 		logger.info("hello 신혁재");
-		logger.info("아무거나 추가");
 		logger.info("브랜치 초기화 : 특정한 커밋의 상태로 완전히 되돌려 버리는 것.");
 		logger.info("커밋 되돌리기(리버스 커밋) : 특정 커밋에서 행한 작업을 취소하여 커밋을 추가하는 것.");
+
 		return "{\"msg\":\"OK\"}";
 	}
 	
