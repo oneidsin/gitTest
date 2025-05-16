095b81dba361aa887552024fab5c5d553e3be94b
oneidsin <oneidsin@naver.com>
2025-05-16 16:03:55 +0900

    업데이트 추가

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +29,14 @@ public class MainController {
 		return "";
 	}
 	
+	// 신규 개발
+	@PostMapping(value = "/write")
+	public Map<String, Object> write() {
+		return null;
+	}
 	
+	@PostMapping(value = "/update")
+	public ResponseEntity<Resource> update() {
+		return null;
+	}
 }
